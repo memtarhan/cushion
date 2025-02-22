754bd3b3b586fb993e02d1fc07083aa5bf15511b
Mehmet Tarhan <hi@memtarhan.com>
2025-02-22 11:50:35 +0000

    <utils>: create project

---
+++ Cushion/CushionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CushionApp.swift
+//  Cushion
+//
+//  Created by Mehmet Tarhan on 22.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct CushionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
